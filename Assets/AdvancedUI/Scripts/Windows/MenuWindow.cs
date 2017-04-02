eec5b4559068ed34d423a6a1bf066968ac9aa941
Razi Abbasi <RaaziAbbasi@Gmail.com>
2017-04-02 19:42:06 -0400

    Menu Done

---
+++ Assets/AdvancedUI/Scripts/Windows/MenuWindow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWindow : GenericWindow {
+
+    public void StartGame() {
+        Debug.Log("Clicked Start Game");
+        manager.Open(4);
+    }
+
+    public void SetCharacters() {
+        Debug.Log("Clicked Set Character");
+        manager.Open(0);
+    }
+
+    public void ViewRecord() {
+        Debug.Log("Clicked View Game");
+        manager.Open(0);
+    }
+
+}
