0813def2d989aa2b997835614c7c66e30eaef738
Razi Abbasi <RaaziAbbasi@Gmail.com>
2017-04-02 22:46:34 -0400

    User Record Window Created

---
+++ Assets/AdvancedUI/Scripts/Windows/RecordWindow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordWindow : GenericWindow {
+
+    public Text WinsValue;
+    public Text LossesValue;
+    public Text StreakValue;
+    public Text ScoreValue;
+
+    public void UpdateValues() {
+        // get values from server;
+
+        // WinsValue.text = ;
+        // LossesValue.text = "";
+        // StreakValue.text = "";
+        // ScoreValue.text = "";
+    }
+
+    public void ClearText() {
+        WinsValue.text = "";
+        LossesValue.text = "";
+        StreakValue.text = "";
+        ScoreValue.text = "";
+    }
+
+    public override void Open() {
+        ClearText();
+        UpdateValues();
+        base.Open();
+    }
+
+    public void BackButton() {
+        OnPreviousWindow();
+    }
+
+}
