ab347a1f79d76dc5ffa8f86fab2c04e9b5844705
theodoretan <tfjtan@hotmail.com>
2017-04-03 08:41:36 -0400

    bam

--- Assets/AdvancedUI/Scripts/Windows/MultiplayerBattleWindow.cs
+++ Assets/AdvancedUI/Scripts/Windows/MultiplayerBattleWindow.cs
@@ -30,14 +30,14 @@ public class MultiplayerBattleWindow : GenericWindow {
         Debug.Log(player);
         Debug.Log(opp);
         Debug.Log("Setting up battle!");
-		this.Player1 = Int32.Parse(player ["id"].str) == 1 	? KnightTemplate.Clone<Actor>() : SlimeTemplate.Clone<Actor>();
-		this.Opp1 = Int32.Parse(opp ["id"].str) == 1 ? KnightTemplate.Clone<Actor>() : SlimeTemplate.Clone<Actor>();
+		this.Player1 = Int32.Parse(player ["character1"]["id"].str) == 1 	? KnightTemplate.Clone<Actor>() : SlimeTemplate.Clone<Actor>();
+		this.Opp1 = Int32.Parse(opp ["character1"]["id"].str) == 1 ? KnightTemplate.Clone<Actor>() : SlimeTemplate.Clone<Actor>();
 
 		this.OpponentHP.text = Opp1.health+"/"+Opp1.maxHealth;
 		this.PlayerHP.text = Player1.health + "/" + Player1.maxHealth;
 
-		this.PlayerImage.sprite = Int32.Parse (player ["id"].str) == 1 ? KnightImageTemplate : SlimeImageTemplate;
-		this.OppImage.sprite = Int32.Parse (opp ["id"].str) == 1 ? KnightImageTemplate : SlimeImageTemplate;
+		this.PlayerImage.sprite = Int32.Parse (player ["character1"]["id"].str) == 1 ? KnightImageTemplate : SlimeImageTemplate;
+		this.OppImage.sprite = Int32.Parse (opp ["character1"]["id"].str) == 1 ? KnightImageTemplate : SlimeImageTemplate;
 	}
 
 
